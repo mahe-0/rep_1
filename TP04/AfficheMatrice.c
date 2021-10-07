b1cdbc1b6e227feb6feee89a8fcf8d3c559ab465
grandisson mahe <grm3871a@u1-204-05l.ens.fsi>
2021-10-07 16:28:53 +0200

    Fin du TP

---
+++ TP04/AfficheMatrice.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+void AfficheMatrice(float** M,int n,int m){
+    for(int i =0;i<n;i++){
+        printf("( ");
+        for(int j=0;j<m;j++){
+            printf(" %5.2f ",M[i][j]);
+        }
+        printf(" )\n");
+    }
+    
+}
+#ifdef TESTS
+#include "matrices.h"
+int
+main(){
+    float**M= CreerMatrice(2,2);
+    M[0][0]=1;
+    M[0][1]= 2;
+    M[1][0]=3;
+    M[1][1]=4;
+    AfficheMatrice(M,2,2);
+    return 0;
+}
+#endif
+
+
+    
+    
+    
+    
--- TP04/CreerMatrice.c
+++ TP04/CreerMatrice.c
@@ -1,21 +1,26 @@
-#include <stdio.h>
-float** CreeMatrice(int n,int m){
+#include <stdlib.h>
+#include "matrices.h"
+float** CreerMatrice(int n,int m){
     float**M;
     M= malloc(n*sizeof(float*));
     for(int i=0;i<n;i++){
-        *M= malloc(n*sizeof(float));
-        for(i=0;i<m;i++);
-        M= malloc(m*sizeof(float));
-        }
-        return M;
+        M[i] = malloc(m*sizeof(float));
+    }
+    return M;
 }
         
+
 #ifdef TESTS
 #include <stdio.h>
+
 int
 main(){
-    int n = 3;
-    int m = 2;
-    CreerMatrice(n,m);
+    float**M=CreerMatrice(2,2);
+    M[0][0]=1;
+    M[0][1]= 2;
+    M[1][0]=3;
+    M[1][1]=4;
+    AfficheMatrice(M,2,2);
+return 0;
 }
 #endif
